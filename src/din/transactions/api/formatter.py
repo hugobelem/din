0894f42b9765a345d82570b26e457934688d73b5
Hugo Belém <belem.hugo@hotmail.com>
2026-03-04 13:33:18 -0300

    feat: add color to cli

--- src/din/transactions/api/formatter.py
+++ src/din/transactions/api/formatter.py
@@ -1,10 +1,33 @@
+from datetime import date
 from din.transactions.core.entity import Transaction
+
+
+BLACK = "\033[30m"
+GRAY = "\033[37m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+BOLD = "\033[1m"
+RESET = "\033[0m"
+
+
+def color(t: Transaction) -> str:
+    today = date.today()
+
+    color = BLACK
+    if t.due < today:
+        color = GRAY
+    elif t.due == today:
+        color = BOLD
+
+    return color
 
 def single(
     t: Transaction,
     category_width: int | None = None,
     amount_width: int | None = None,
-):
+):      
     amount = f'{t.amount / 100:.2f}'
 
     if category_width is None and amount_width is None:
@@ -35,4 +58,4 @@ def multiple(transactions: list[Transaction]):
             category_width=category_width,
             amount_width=amount_width,
         )
-        print(f'{i + 1}. {line}')
+        print(f'{color(t)}{i + 1}. {line}{RESET}')
