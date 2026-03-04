df78e1d8c01223c3ebbf865f8ab50406843b002a
Hugo Belém <belem.hugo@hotmail.com>
2026-03-04 12:52:13 -0300

    chore: add created datetime to transaction cli get cmd

--- src/din/transactions/api/formatter.py
+++ src/din/transactions/api/formatter.py
@@ -5,26 +5,27 @@ def single(
     category_width: int | None = None,
     amount_width: int | None = None,
 ):
-    amount = f"{t.amount / 100:.2f}"
+    amount = f'{t.amount / 100:.2f}'
 
     if category_width is None and amount_width is None:
         return (
-            f"{t.id} | {t.due} | [ {t.type.value} ] | "
-            f"{t.category} | {amount} | {t.description}"
+            f'{t.created.strftime('%a %-d %b %H:%M %Y')} | '
+            f'{t.id} | {t.due} | [ {t.type.value} ] | '
+            f'{t.category} | {amount} | {t.description}'
         )
 
     return (
-        f"{t.id} | {t.due} | [ {t.type.value:7} ] | "
-        f"{t.category:<{category_width}} | "
-        f"{amount:>{amount_width}} | "
-        f"{t.description}"
+        f'{t.id} | {t.due} | [ {t.type.value:7} ] | '
+        f'{t.category:<{category_width}} | '
+        f'{amount:>{amount_width}} | '
+        f'{t.description}'
     )
 
 def multiple(transactions: list[Transaction]):
     if not transactions:
         return
 
-    formatted_amounts = [f"{t.amount / 100:.2f}" for t in transactions]
+    formatted_amounts = [f'{t.amount / 100:.2f}' for t in transactions]
     amount_width = max(len(a) for a in formatted_amounts)
     category_width = max(len(t.category) for t in transactions)
 
@@ -34,4 +35,4 @@ def multiple(transactions: list[Transaction]):
             category_width=category_width,
             amount_width=amount_width,
         )
-        print(f"{i + 1}. {line}")
+        print(f'{i + 1}. {line}')
