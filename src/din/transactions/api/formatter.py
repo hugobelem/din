a44a0f7777d639b1244beb81f49e6a8547b70c53
Hugo Belém <belem.hugo@hotmail.com>
2026-03-04 14:45:39 -0300

    feat(cli): sort transaction by month + add month balance

--- src/din/transactions/api/formatter.py
+++ src/din/transactions/api/formatter.py
@@ -1,4 +1,3 @@
-from datetime import date
 from din.transactions.core.entity import Transaction
 
 
@@ -10,18 +9,6 @@ YELLOW = "\033[33m"
 BLUE = "\033[34m"
 BOLD = "\033[1m"
 RESET = "\033[0m"
-
-
-def color(t: Transaction) -> str:
-    today = date.today()
-
-    color = BLACK
-    if t.due < today:
-        color = GRAY
-    elif t.due == today:
-        color = BOLD
-
-    return color
 
 def single(
     t: Transaction,
@@ -52,10 +39,35 @@ def multiple(transactions: list[Transaction]):
     amount_width = max(len(a) for a in formatted_amounts)
     category_width = max(len(t.category) for t in transactions)
 
-    for i, t in enumerate(transactions):
-        line = single(
-            t,
-            category_width=category_width,
-            amount_width=amount_width,
-        )
-        print(f'{color(t)}{i + 1}. {line}{RESET}')
+    month = ''
+    group_by_month: dict[str, list[Transaction]] = {}
+
+    for t in transactions:
+        month_name = t.due.strftime('%b')
+
+        if month_name != month:
+            month = month_name
+            group_by_month.update({month: []})
+
+        if month_name in group_by_month.keys():
+            group_by_month[month_name].append(t)
+
+    for month, transactions in reversed(group_by_month.items()):
+        print(f'{BOLD}{month}{RESET}')
+        month_balance = 0
+        for i, t in enumerate(transactions):
+            line = single(
+                t,
+                category_width=category_width,
+                amount_width=amount_width,
+            )
+            print(f'{i + 1:3}. {line}')
+            month_balance += t.amount
+
+        if month_balance > 0:
+            balance_color = GREEN
+        else:
+            balance_color = RED
+
+        print(f'{balance_color}balance: {month_balance / 100:.2f}{RESET}')
+        print('')
