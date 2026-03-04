d9ac6e8036722a3ba3a2f874ac0cb3f8fd45b4f7
Hugo Belém <belem.hugo@hotmail.com>
2026-03-04 09:09:29 -0300

    chore: rename variables

--- src/din/interface/cli.py
+++ src/din/interface/cli.py
@@ -1,10 +1,10 @@
 import typer
-from . import transaction, balance, income, expense, total
+from . import transaction_app, balance_app, income_app, expense_app, total_app
 
 
 app = typer.Typer(no_args_is_help=True)
-app.add_typer(transaction, name='t')
-app.add_typer(balance, name='balance')
-app.add_typer(income, name='income')
-app.add_typer(expense, name='expense')
-app.add_typer(total, name='total')
+app.add_typer(transaction_app, name='t')
+app.add_typer(balance_app, name='balance')
+app.add_typer(income_app, name='income')
+app.add_typer(expense_app, name='expense')
+app.add_typer(total_app, name='total')
